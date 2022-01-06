9da2b52172c02146fa6bd306a7015eda5e1172f7
Juha Aarnio <juha.aarnio@live.fi>
2022-01-06 22:13:18 +0200

    Added effects file
    Added a file for periodic effects, implemented an example with mana regen.

---
+++ src/effects.rs
@@ -0,0 +1,3 @@
+pub fn mana_regen(mana_return: i32) -> i32{
+    return mana_return;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod priority_list;
 mod character;
+pub mod effects;
 
 fn main() {
     let priority_list = priority_list::execute_action();
@@ -8,17 +9,21 @@ fn main() {
     let runtime = runtime_input * 1000;
     let mana_cost = priority_list[0].mana_cost * test_character.max_mana as f32;
     let avg_num_of_targets = 3;
+    let mana_regen_interval_starting_value = 5000;
     let mut cast_time = priority_list[0].cast_time;
     let mut time = 0; 
     let mut total_healing: f32 = 0.0;
+    let mut mana_regen_interval: i16 = 5000;
     
     while time < runtime {
         time += 1; 
         cast_time -= 1;
+        mana_regen_interval -= 1;
         stat_conversion(test_character.crit_rating, test_character.mastery_rating, test_character.haste_rating, test_character.leech_rating, 
                         test_character.speed_rating, test_character.versatility_rating);
         if cast_time == 0 && test_character.mana as f32 > mana_cost {
             println!("{:?}", priority_list[0]);
+            println!("{:?}", test_character.mana);
             cast_time = priority_list[0].cast_time;
             test_character.mana -= mana_cost as i32; 
             total_healing += (priority_list[0].healing_coeff * test_character.int as f32) * avg_num_of_targets as f32;
@@ -26,6 +31,10 @@ fn main() {
                 println!("out of mana");
                 break;
             }
+        }
+        if mana_regen_interval == 0 {
+            test_character.mana += effects::mana_regen(1200);
+            mana_regen_interval = mana_regen_interval_starting_value;
         }
     }
 
@@ -46,7 +55,7 @@ fn stat_conversion(crit: i32, mastery: i32, haste: i32, leech: i32, speed: i32, 
     let haste_percentage = haste as f32 / haste_conversion_rate;
     let versatility_percentage = versatility as f32 / versatility_conversion_rate;
     
-    let mut conversion_rates: Vec<f32>;
+    let mut conversion_rates: Vec<f32> = Vec::new();
     conversion_rates.push(crit_percentage);
     conversion_rates.push(mastery_percentage);
     conversion_rates.push(haste_percentage);
