c54b5997092d848f809508bed38d1f13313566fe
Juha Aarnio <juha.aarnio@live.fi>
2022-02-01 21:20:45 +0200

    Update main.rs
    Refactored the code to prevent a crash when out of mana

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let runtime_input = 600;
     let runtime = runtime_input * 1000;
     let mana_cost = priority_list[0].mana_cost * test_character.max_mana as f32;
-    let avg_num_of_targets = 3;
+    let avg_num_of_targets = 4;
     let mana_regen_interval_starting_value = 5000;
     let mut cast_time = priority_list[0].cast_time;
     let mut time = 0; 
@@ -17,20 +17,22 @@ fn main() {
     
     while time < runtime {
         time += 1; 
-        cast_time -= 1;
         mana_regen_interval -= 1;
         stat_conversion(test_character.crit_rating, test_character.mastery_rating, test_character.haste_rating, test_character.leech_rating, 
                         test_character.speed_rating, test_character.versatility_rating);
-        if cast_time == 0 && test_character.mana as f32 > mana_cost {
-            println!("{:?}", priority_list[0]);
-            println!("{:?}", test_character.mana);
-            cast_time = priority_list[0].cast_time;
-            test_character.mana -= mana_cost as i32; 
-            total_healing += (priority_list[0].healing_coeff * test_character.int as f32) * avg_num_of_targets as f32;
+        if test_character.mana as f32 > mana_cost {
+            cast_time -= 1;
+            if cast_time == 0 {
+                println!("{:?}", priority_list[0]);
+                println!("{:?}", test_character.mana);
+                cast_time = priority_list[0].cast_time;
+                test_character.mana -= mana_cost as i32; 
+                total_healing += (priority_list[0].healing_coeff * test_character.int as f32) * avg_num_of_targets as f32;
             if test_character.mana < mana_cost as i32 {
                 println!("out of mana");
-                break;
+                cast_time = priority_list[0].cast_time;
             }
+        } 
         }
         if mana_regen_interval == 0 {
             test_character.mana += effects::mana_regen(1200);
